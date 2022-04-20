6d03e830013b9bd72b6efd548059908b128d324a
victoryhwan7 <victoryhwan7@gmail.com>
2022-04-20 09:21:42 +0900

    test

--- routes/goods.js
+++ routes/goods.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/test', function (req, res, next) {
-	res.render('test');
+	res.render('test'); //test
 });
 
 router.get('/jquery', function (req, res, next) {
