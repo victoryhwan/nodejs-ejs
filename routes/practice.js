cd992932574a841475230eb9bca04c5e769da631
victoryhwan7 <victoryhwan7@gmail.com>
2022-05-11 19:24:57 +0900

    스켈레톤 UI 적용

--- routes/practice.js
+++ routes/practice.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const PullToRefresh = require('pulltorefreshjs');
+
 
 router.get('/test', function (req, res, next) {
 	res.render('test'); //test
 });
+
+router.get('/skeleton', function (req, res, next) {
+	res.render('skeletonBox'); //test
+});
+
 
 router.get('/jquery', function (req, res, next) {
 	res.render('jquery');
